3236e082fb59753e20c83becdfa32ff11c9f3157
linxiaodan <linxiaodan>
2017-07-13 17:27:42 +0800

    s

--- static/js/a.js
+++ static/js/a.js
@@ -1,3 +1,15 @@
-document.write('aaabbb')
-document.write('<p>aaa</p>')
+document.write('aaabbb');
+document.write('<p>aaa</p>');
 
+var person = {
+    name: 'Bob',
+    age: 20,
+    tags: ['js', 'web', 'mobile'],
+    city: 'Beijing',
+    hasCar: true,
+    zipcode: null
+};
+document.write(person.age+'<br>');
+document.write(person.name+'<br>');
+
+alert('');
